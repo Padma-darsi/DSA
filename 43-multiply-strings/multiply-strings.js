79d2b1887caffaef5af42564efb54b65db88a161
Padma Darsi <125295009+Padma-darsi@users.noreply.github.com>
2026-03-31 19:31:40 +0530

    Time: 5 ms (67.31%) | Memory: 54.9 MB (70.73%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    if (num1 === "0" || num2 === "0") return "0";
+
+    let m = num1.length;
+    let n = num2.length;
+
+    let result = new Array(m + n).fill(0);
+
+    for (let i = m - 1; i >= 0; i--) {
+        for (let j = n - 1; j >= 0; j--) {
+
+            let mul = (num1[i] - '0') * (num2[j] - '0');
+
+            let sum = mul + result[i + j + 1];
+
+            result[i + j + 1] = sum % 10;
+            result[i + j] += Math.floor(sum / 10);
+        }
+    }
+
+    // Convert to string and remove leading zeros
+    return result.join('').replace(/^0+/, '');
+};
