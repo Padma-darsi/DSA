869e7e678373288ce1203f2d2e1c0380cb6ac848
Padma Darsi <125295009+Padma-darsi@users.noreply.github.com>
2026-04-01 16:10:47 +0530

    Time: 2 ms (62.30%) | Memory: 53.8 MB (96.07%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let jumps = 0;
+    let currentEnd = 0;
+    let farthest = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+
+        // When we reach end of current jump range
+        if (i === currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+};
